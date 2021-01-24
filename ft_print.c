6afdf01d5a7f33ad13828a5cddcff486b9edf9a5
samilim <77201910+samilim@users.noreply.github.com>
2021-01-24 03:14:50 +0100

    Create ft_print.c

---
+++ ft_print.c
@@ -0,0 +1,5 @@
+
+BONUS
+Gérez une ou plusieurs des conversions suivantes : nfge
+• Gérez un ou plusieurs des flags suivants : l ll h hh
+• Gérez tous les flags suivants : ’# +’ (oui espace est un flag valide)
